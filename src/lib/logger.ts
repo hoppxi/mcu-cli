8e06f566dbbda132a089901d64eddf5426d8a4b9
hoppxi <hoppxii@gmail.com>
2025-08-22 19:35:35 +0300

    feat(logger): add option to show time and to disable logging totally

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,16 +1,67 @@
 import chalk from "chalk";
 
 export class Logger {
-  info(msg: string) {
-    console.log(chalk.blueBright("[INFO]"), msg);
+  private _enableLogging: boolean = false;
+  private _showTimestamp: boolean;
+
+  constructor(showTimestamp = false) {
+    this._showTimestamp = showTimestamp;
   }
-  warn(msg: string) {
-    console.warn(chalk.yellowBright("[WARN]"), msg);
+
+  set enabled(value: boolean) {
+    this._enableLogging = value;
   }
-  error(msg: string) {
-    console.error(chalk.redBright("[ERROR]"), msg);
+
+  get enabled(): boolean {
+    return this._enableLogging;
   }
-  success(msg: string) {
-    console.log(chalk.greenBright("[OK]"), msg);
+
+  private _log(
+    type: "info" | "warn" | "error" | "success",
+    msg: string,
+    force = false
+  ) {
+    if (!this._enableLogging && !force) return;
+
+    const prefixMap = {
+      info: chalk.blueBright("[INFO]"),
+      warn: chalk.yellowBright("[WARN]"),
+      error: chalk.redBright("[ERROR]"),
+      success: chalk.greenBright("[OK]"),
+    };
+
+    const timestamp = this._showTimestamp
+      ? chalk.gray(`[${new Date().toLocaleTimeString()}]`)
+      : "";
+    const prefix = `${timestamp} ${prefixMap[type]}`;
+
+    switch (type) {
+      case "info":
+      case "success":
+        console.log(prefix, msg);
+        break;
+      case "warn":
+        console.warn(prefix, msg);
+        break;
+      case "error":
+        console.error(prefix, msg);
+        break;
+    }
+  }
+
+  info(msg: string, force = false) {
+    this._log("info", msg, force);
+  }
+
+  warn(msg: string, force = false) {
+    this._log("warn", msg, force);
+  }
+
+  error(msg: string, force = false) {
+    this._log("error", msg, force);
+  }
+
+  success(msg: string, force = false) {
+    this._log("success", msg, force);
   }
 }
